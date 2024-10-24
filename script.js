450c4e682158f02eae792201cae9c3ead74a112b
Arif Akbar <arifditlin@gmail.com>
2024-10-24 18:34:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('slide-active');
+        if (i === index) {
+            slide.classList.add('slide-active');
+        }
+    });
+}
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide(slideIndex);
+}
+
+// Tampilkan slide pertama saat halaman dimuat
+showSlide(slideIndex);
